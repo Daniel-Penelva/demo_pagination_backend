95e70ae36e646e91856e127995c9bdf431e15f4f
Daniel Penelva <d4n.andrade@gmail.com>
2025-05-13 19:30:30 -0300

    feat: criando projeto Spring Boot com Paginação

---
+++ src/main/java/com/api/demo_pagination_backend/DemoPaginationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.api.demo_pagination_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPaginationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPaginationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/demo_pagination_backend/DemoPaginationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.demo_pagination_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPaginationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
